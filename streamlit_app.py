dbfb78135a9558ca519ed166e65fd714bff9904c
foobar-nordquist <135115505+foobar-nordquist@users.noreply.github.com>
2023-06-02 18:32:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # create the repeatable code block called a function
-def get_fruityvice_data(this_fruit_choise):
+def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
