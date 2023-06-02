dc0866c302697839174681423974b33cdab50045
foobar-nordquist <135115505+foobar-nordquist@users.noreply.github.com>
2023-06-02 18:45:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,27 +44,33 @@ try:
 except URLError as e:
   streamlit.error()
   
-streamlit.write('The user entered ', fruit_choice)
-
+streamlit.header("The fruit load list contains:")
+# SNOWFLAKE-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+  
+# Add button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
+  streamlit.write('The user entered ', fruit_choice)
 # --import requests
-
-
-# Take JSON version of the response and normalize it
-
-# write your own comment - what does this do?
-
 
 # DONT RUN ANYTHING PAST HERE 
 streamlit.stop()
 
 # --import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
+# my_cur = my_cnx.cursor()
+
+ 
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+
 
 # Allow end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
